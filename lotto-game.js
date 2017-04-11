af27b09891e6414bfa129e317ff1babf93b65a7c
Chris Thurber <cthurber@me.com>
2017-04-11 02:23:33 -0400

    Added Scenario simulation to game

--- lotto-game.js
+++ lotto-game.js
@@ -10,7 +10,7 @@
 
 var cashEarned = 0;
 var choices = [];
-var Scenarios = [];
+var scenarios = [];
 var randChoices = [];
 
 // Returns array with ints from x to n
@@ -23,6 +23,11 @@ function iRange(x, n) {
   return a;
 }
 
+// Recreated Python's print function for testing
+function print(string_to_print) {
+  console.log(string_to_print);
+}
+
 // Choice consists of a range of values that will result in a payout
 function Choice(rangeIn, payoutIn) {
   this.range = rangeIn;
@@ -33,7 +38,7 @@ function Choice(rangeIn, payoutIn) {
 
 function randChoice() {
   this.range = iRange(6,10);
-  this.payout = (Math.random()*21).toFixed(2);
+  this.payout = parseFloat(Math.random()*21).toFixed(2);
   this.last = this.range.length - 1;
   this.description = this.range.length == 1 ? "If the die is " + this.range[0] + " then get $" + this.payout : "If the die is between " + this.range[0] + " and " + this.range[this.last] + " then get $" + this.payout;
 }
@@ -45,49 +50,51 @@ function Scenario(choiceA, choiceB) {
   this.chosen = null;
 }
 
-// Sim simulates a two-dice roll and returns whether or not the result is in the range of the 'choice' selected
-function Sim(choice) {
+// Generates the roll of two die
+function Roll() {
   var diOne = Math.floor(Math.random()*7);
   var diTwo = Math.floor(Math.random()*7);
   var outcome = diOne + diTwo;
 
+  return outcome;
+}
+
+// Compares the result of the roll of two die to the chosen range
+function Sim(choice) {
+  var outcome = Roll();
   return choice.range.includes(outcome);
 }
 
-// Simulates an array of choices and returns the payouts accordinglys
-function SimAll(choices) {
+// Simulates an array of scenarios and returns the payouts accordingly
+function SimScenarios(scenarios) {
   var payouts = 0;
   var i = 0;
-  for (i = 0; i < choices.length; i++) {
-    console.log("Payout:"+choices[])
-    if(Sim(choices[i])) {
-      payouts += choices.pop().payout;
+  var roll = Roll();
+  print(roll);
+
+  for (i = 0; i < scenarios.length; i++) {
+    print("\t Choice: " + scenarios[i].chosen.range + " for " +scenarios[i].chosen.payout);
+    if(scenarios[i].chosen.range.includes(roll)) {
+      payouts = scenarios.pop().chosen.payout + payouts;
     }
   }
-
-  return payouts;
+  return parseFloat(payouts).toFixed(2);
 }
 
 // Allows choice at index to be chosen
 function Choose(index, choice) {
   if(choice === 0) {
-    choices.push(Scenarios[index].choiceA);
+    scenarios[index].chosen = scenarios[index].choiceA;
   } else {
-    choices.push(Scenarios[index].choiceB);
+    scenarios[index].chosen = scenarios[index].choiceB;
   }
 }
 
 // Calculates game total and adds to running total
 function submitForm() {
+    cashEarned = SimScenarios(scenarios) + cashEarned;
+    print(cashEarned);
 
-    for (q = 0; q < Scenarios.length; q++) {
-      choices.push(Scenarios[q].chosen);
-    }
-
-    cashEarned += SimAll(choices);
-
-    console.log($(this).serialize()); // will be the serialized form
-    $(this).append($(this).serialize() + '<br />');
     $('#cash-earned').html(cashEarned);
 }
 
@@ -99,19 +106,19 @@ for (r = 0; r < 10; r++) {
 // This section will be replaced with code that creates scenario objects with data from database
 // Creates sample array of scenarios
 for (m = 0; m < 5; m++) {
-  Scenarios.push(new Scenario(randChoices.pop(), randChoices.pop()));
+  scenarios.push(new Scenario(randChoices.pop(), randChoices.pop()));
 }
 
 // Add choices to page
 $(document).ready(function() {
     var pageList = "";
     var i = 0;
-    for (i = 0; i < Scenarios.length; i++) {
+    for (i = 0; i < scenarios.length; i++) {
       pageList += '<tr><td><button onclick="Choose('+i+',0)" id="'+i+'-A" name="'+i+'" value="A">';
-      pageList += Scenarios[i].choiceA.description;
+      pageList += scenarios[i].choiceA.description;
       pageList += '</button></td>';
       pageList += '<td><button onclick="Choose('+i+',1)" id="'+i+'-B" name="'+i+'" value="B">';
-      pageList += Scenarios[i].choiceB.description;
+      pageList += scenarios[i].choiceB.description;
       pageList += '</button></td></tr>';
     }
     $('#choice-selection').html(pageList);
